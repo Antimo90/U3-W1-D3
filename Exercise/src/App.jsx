6c997f6323b42b1016f66f846b8cde34c4bbe7f3
Antimo <antimo6@icloud.com>
2025-07-23 17:00:36 +0200

    exercise done

--- Exercise/src/App.jsx
+++ Exercise/src/App.jsx
@@ -4,16 +4,27 @@ import MyNav from "./components/MyNav";
 import MyFooter from "./components/MyFooter";
 import Welcome from "./components/Welcome";
 // import AllTheBooks from "./components/AllTheBooks";
-import SingleBook from "./components/SingleBook";
+
 import fantasy from "../src/data/fantasy.json";
+import history from "../src/data/history.json";
+import horror from "../src/data/horror.json";
+import romance from "../src/data/romance.json";
+import scifi from "../src/data/scifi.json";
+import BookList from "./components/BookList";
 
 function App() {
-  const bookToDisplay = fantasy[0];
+  const allBooksForBookList = [
+    ...fantasy,
+    ...history,
+    ...horror,
+    ...romance,
+    ...scifi,
+  ];
   return (
     <>
       <MyNav />;
       <Welcome />
-      <SingleBook book={bookToDisplay} />
+      <BookList books={allBooksForBookList} />
       <MyFooter />
     </>
   );
